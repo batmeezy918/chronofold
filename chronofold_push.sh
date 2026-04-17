9c4079f3078646a699aa4db29cca768a655dcf2e
Batmeezy918 <batmeezy2024@gmail.com>
2026-04-16 19:02:31 -0500

    Init commit 1776384151

--- chronofold_push.sh
+++ chronofold_push.sh
@@ -8,6 +8,4 @@ git add .
 
 git commit -m "Init commit $(date +%s)" || echo "No changes"
 
-git push https://$(cat ~/.git_token)@github.com/batmeezy918/chronofold.git
-
-echo "[ChronoFold] DONE"
+git push https://batmeezy918:$(cat ~/.git_token)@github.com/batmeezy918/chronofold.git
