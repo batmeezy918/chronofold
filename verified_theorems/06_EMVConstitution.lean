b3d09d9de8a35472ca244aeb58bd25ba481c2949
batmeezy918 <batmeezy2024@gmail.com>
2026-09-05 06:24:56 -0500

    Add verified theorem 06 proof kernel

---
+++ verified_theorems/06_EMVConstitution.lean
@@ -0,0 +1,20 @@
+import Chronofold.EmvConstitution
+
+namespace Verified_06_EMVConstitution
+
+ theorem annotate_admissible := Chronofold.EMV.annotate_admissible
+ theorem stampSequence_admissible := Chronofold.EMV.stampSequence_admissible
+ theorem sealOverlay_admissible := Chronofold.EMV.sealOverlay_admissible
+ theorem advance_wellDefined_phase := Chronofold.EMV.advance_wellDefined_phase
+ theorem advance_not_always_admissible := Chronofold.EMV.advance_not_always_admissible
+ theorem transition_not_wellDefined := Chronofold.EMV.transition_not_wellDefined
+ theorem transition_not_admissible := Chronofold.EMV.transition_not_admissible
+ theorem criticalFailed_factors_pi := Chronofold.EMV.criticalFailed_factors_pi
+ theorem criticalApproved_factors_pi := Chronofold.EMV.criticalApproved_factors_pi
+ theorem emv_master_closure := Chronofold.EMV.emv_master_closure
+ theorem emv_maximal_abstract := Chronofold.EMV.emv_maximal_abstract
+ theorem emv_agd_maximal := Chronofold.EMV.emv_agd_maximal
+ theorem emv_overlays_closed := Chronofold.EMV.emv_overlays_closed
+ theorem emv_overlay_TBar_is_id := Chronofold.EMV.emv_overlay_TBar_is_id
+
+end Verified_06_EMVConstitution
