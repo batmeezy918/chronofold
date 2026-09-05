52126906f4a8cc8f7e8e6309fb21b1a165152d3e
batmeezy918 <batmeezy2024@gmail.com>
2026-09-05 06:25:16 -0500

    Add verified theorem 09 proof kernel

---
+++ verified_theorems/09_AutoOmega.lean
@@ -0,0 +1,9 @@
+import Chronofold.AutoOmega
+
+namespace Verified_09_AutoOmega
+
+ theorem omega_divides_n := Chronofold.AutoOmega.omega_divides_n
+ theorem omega_nonneg := Chronofold.AutoOmega.omega_nonneg
+ theorem omega_le_n := Chronofold.AutoOmega.omega_le_n
+
+end Verified_09_AutoOmega
