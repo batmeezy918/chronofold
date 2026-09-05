b8606f958b2c8f80f11ea472263070f1d9d1b282
batmeezy918 <batmeezy2024@gmail.com>
2026-09-05 06:25:54 -0500

    Add verified omega theorem kernel

---
+++ verified_theorems/12_THM_000003_omega_divides_n.lean
@@ -0,0 +1,9 @@
+import Chronofold.AutoOmega
+
+namespace Verified_12_THM_000003
+
+theorem omega_divides_n (x n : Nat) :
+    Chronofold.AutoOmega.omega x n ∣ n :=
+  Chronofold.AutoOmega.omega_divides_n x n
+
+end Verified_12_THM_000003
