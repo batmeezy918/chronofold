cd48fb8d1f7ad4dc80d9e5d86469a58b9dd5082e
batmeezy918 <batmeezy2024@gmail.com>
2026-09-05 06:25:09 -0500

    Add verified theorem 08 proof kernel

---
+++ verified_theorems/08_CvrPhase0.lean
@@ -0,0 +1,19 @@
+import Chronofold.CvrPhase0
+
+namespace Verified_08_CvrPhase0
+
+ theorem namespace_preservation := Chronofold.CvrPhase0.namespace_preservation
+ theorem version_preservation := Chronofold.CvrPhase0.version_preservation
+ theorem provenance_preservation := Chronofold.CvrPhase0.provenance_preservation
+ theorem relationship_resolution_preservation := Chronofold.CvrPhase0.relationship_resolution_preservation
+ theorem acyclicity_preservation := Chronofold.CvrPhase0.acyclicity_preservation
+ theorem omega_preservation := Chronofold.CvrPhase0.omega_preservation
+ theorem admissibility_closure := Chronofold.CvrPhase0.admissibility_closure
+ theorem operator_composition_closed := Chronofold.CvrPhase0.operator_composition_closed
+ theorem identity_operator_admissible := Chronofold.CvrPhase0.identity_operator_admissible
+ theorem operator_composition_associative := Chronofold.CvrPhase0.operator_composition_associative
+ theorem inverse_is_admissible := Chronofold.CvrPhase0.inverse_is_admissible
+ theorem defect_monotonicity := Chronofold.CvrPhase0.defect_monotonicity
+ theorem constitutional_closure := Chronofold.CvrPhase0.constitutional_closure
+
+end Verified_08_CvrPhase0
