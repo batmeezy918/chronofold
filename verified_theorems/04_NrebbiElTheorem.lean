a98b40517295f02719c91388089943e23dce2f33
batmeezy918 <batmeezy2024@gmail.com>
2026-09-05 06:24:43 -0500

    Add verified theorem 04 proof kernel

---
+++ verified_theorems/04_NrebbiElTheorem.lean
@@ -0,0 +1,15 @@
+import Chronofold.NrebbiElTheorem
+
+namespace Verified_04_NrebbiElTheorem
+
+ theorem nrebbi_el_theorem := NrebbiEl.nrebbi_el_theorem
+ theorem descent_iff_recursive := NrebbiEl.descent_iff_recursive
+ theorem admissible_iff_identity_descent := NrebbiEl.admissible_iff_identity_descent
+ theorem descends_implies_wellDefined := NrebbiEl.descends_implies_wellDefined
+ theorem admissible_iterate := NrebbiEl.admissible_iterate
+ theorem descends_compose := NrebbiEl.descends_compose
+ theorem reconstruction_is_section := NrebbiEl.reconstruction_is_section
+ theorem conjugate_of_descent := NrebbiEl.conjugate_of_descent
+ theorem observable_factors := NrebbiEl.observable_factors
+
+end Verified_04_NrebbiElTheorem
